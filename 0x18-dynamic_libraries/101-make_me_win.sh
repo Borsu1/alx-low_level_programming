73ab154b522ae98f219cce574624c4db8e96c04b
Borsu1 <borsudaniel@gmail.com>
2023-12-14 09:38:30 -0800

    Make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://githubusercontent.com/Borsu1/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD="$PWD/../libtest.so"
+wget -p .. https://githubusercontent.com/Borsu1/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
