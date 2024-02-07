ee66a455f25eadc7c03626845e8bf53a5e599d74
Borsu1 <borsudaniel@gmail.com>
2024-02-07 10:19:46 -0800

    Doubly linked lists updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	while (temp != NULL)
 	{
-	if (i == idx)
+	if (i + 1 == idx)
 	{
 	new_node->next = temp;
 	new_node->prev = temp->prev;
@@ -45,6 +45,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	i++;
 	temp = temp->next;
+	}
+
+	if (i == idx)
+	{
+		new_node->next = NULL;
+		new_node->prev = temp;
+		temp->next = new_node;
+		return (new_node);
 	}
 
 	free(new_node);
