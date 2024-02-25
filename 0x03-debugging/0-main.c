fbb2a8ff98555eaa73aba45d537dc99c3c20d397
Borsu1 <borsudaniel@gmail.com>
2024-02-25 04:22:20 -0800

    Updating of debugging files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,9 +5,25 @@
 */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i;
+
+    i = 0;
+    positive_or_negative(i);
+
+    return (0);
 }
 
+/**
+* positive_or_negative - checks if a number is positive, negative or zero
+* @i: the number to check
+* Return: void
+*/
+void positive_or_negative(int i)
+{
+    if (i > 0)
+        printf("%d is positive\n", i);
+    else if (i < 0)
+        printf("%d is negative\n", i);
+    else
+        printf("%d is zero\n", i);
+}
