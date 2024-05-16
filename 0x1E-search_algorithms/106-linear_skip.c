58a5afa3de3e728945f5d15f112f7a26177fc835
Borsu1 <borsudaniel@gmail.com>
2024-05-16 05:37:12 -0700

    Resolving code from segmental fault

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,45 +1,52 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * linear_skip - searches for a value in a sorted skip list of integers
- * @list: pointer to the head of the skip list to search in
- * @value: value to search for
- * Return: pointer on the first node where value is located
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ * @list: A pointer to the head of the skip list to search in.
+ * @value: The value to search for.
+ *
+ * Return: A pointer on the first node where value is located.
+ *         If value is not present in list or if head is NULL, return NULL.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *prev = list;
-	skiplist_t *node = list;
+	skiplist_t *prev, *next;
 
 	if (list == NULL)
 		return (NULL);
 
-	while (node->express != NULL && node->express->n < value)
+	next = list->express;
+	prev = list;
+
+	printf("Value checked at index [%lu] = [%d]\n",
+		next->index, next->n);
+	while (next && next->n < value)
 	{
-		prev = node;
-		node = node->express;
-		printf("Value checked at index [%lu] = [%d]\n",
-			node->index, node->n);
+		prev = next;
+		if (next->express)
+		{
+			next = next->express;
+			printf("Value checked at index [%lu] = [%d]\n",
+				next->index, next->n);
+		}
+		else
+		{
+			while (next->next)
+				next = next->next;
+		}
 	}
 
-	if (node->express != NULL)
-		printf("Value checked at index [%lu] = [%d]\n",
-			node->express->index, node->express->n);
 	printf("Value found between indexes [%lu] and [%lu]\n",
-		prev->index, node->index);
-
-	while (prev != NULL && prev->index <= node->index)
+		prev->index, next->index);
+	while (prev && prev->index <= next->index)
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
 			prev->index, prev->n);
 		if (prev->n == value)
 			return (prev);
-		if (prev->next != NULL)
 		prev = prev->next;
-		else
-			break;
 	}
 
 	return (NULL);
