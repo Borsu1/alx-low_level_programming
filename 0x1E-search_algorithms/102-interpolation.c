fdc291d503697e7525f082075f9fe3794527ee05
Borsu1 <borsudaniel@gmail.com>
2024-05-16 11:26:27 -0700

    Resolving task output

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "search_algos.h"
 
+/**
+ * interpolation_search - Searches for a value in a sorted array of integers
+ * using the Interpolation search algorithm.
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in array.
+ * @value: Value to search for.
+ * Return: First index where value is located, or -1 if value is not found or
+ * if array is NULL.
+ */
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0;
